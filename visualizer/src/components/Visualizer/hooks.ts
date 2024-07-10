c268701c9607f6c1fd0a73adb21dbfa2404f739d
UserNaneless <vgripor@gmail.com>
2024-07-10 16:11:55 +0300

    hook remove

--- visualizer/src/components/Visualizer/hooks.ts
+++ visualizer/src/components/Visualizer/hooks.ts
@@ -1,18 +1,1 @@
-import { useState } from "react";
-
-const useMeasure = () => {
-
-    const [size, setSize] = useState({ width: 0, height: 0 });
-
-    return {
-        ...size,
-        watch: (element: HTMLElement | null) => {
-            if (!element) return
-            setSize(() => ({
-                width: element.clientWidth,
-                height: element.clientHeight
-            }))
-        }
-    }
-}
 
